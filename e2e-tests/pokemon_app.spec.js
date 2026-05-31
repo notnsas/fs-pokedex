467e98bf4f04bc19171df8f2b06bd9a1078bbade
notnsas <ramapanjinararendra@gmail.com>
2026-05-31 17:31:30 +0700

    try to fix test and added new one

--- e2e-tests/pokemon_app.spec.js
+++ e2e-tests/pokemon_app.spec.js
@@ -1,9 +1,17 @@
-import { test, describe, expect } from '@playwright/test'
+import { test, describe, expect, beforeEach } from '@playwright/test'
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:8080')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('the page can be navigated to a specific Pokémon', async ({ page }) => {
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
